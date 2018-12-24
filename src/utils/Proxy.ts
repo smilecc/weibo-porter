36cd4a0281eebfa9ef6c98e11ebce18f691e0419
smilecc <smilexcc@gmail.com>
2018-12-24 20:08:43 +0800

    Modify: 增大获取代理的随机性

--- src/utils/Proxy.ts
+++ src/utils/Proxy.ts
@@ -51,7 +51,7 @@ export class ProxyUtil {
    * @param driver WebDriver
    */
   protected static async getNewProxy(driver: WebDriver): Promise<void> {
-    await driver.get('https://www.xicidaili.com/nn');
+    await driver.get(`https://www.xicidaili.com/nn/${Math.ceil(Math.random() * 100)}`);
     const ipListTable = await driver.wait(until.elementLocated(By.id('ip_list')));
     const trList = await ipListTable.findElements(By.tagName('tr'));
     const trListLength = trList.length;
